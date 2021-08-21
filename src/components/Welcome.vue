d1735e5163a387aa8bb5f85af0f6e09f943bf95d
Clayton A Davis <cad@clayadavis.net>
2021-08-21 14:44:14 -0400

    Add Welcome page

---
+++ src/components/Welcome.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h1 class="jumbo">dice.party</h1>
+    </div>
+
+    <div class="row">
+      <form>
+        <label for="roomname">Room Name</label>
+        <input type="text" id="roomname" />
+        <input type="submit" class="primary" value="Go!" />
+      </form>
+    </div>
+
+    <div class="row">
+      <div class="text">
+        <p>
+          Type in a name and click "Go!" to make your own room where you can
+          roll dice.
+        </p>
+        <p>
+          Your friends can join you by typing the same room name, or you can
+          send them the link from the address bar.
+        </p>
+      </div>
+    </div>
+  </div>
+  <!-- .container -->
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  props: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.jumbo {
+  font-size: 60pt;
+}
+
+.monospace {
+  font-family: monospace;
+}
+
+.text {
+  margin-top: 1em;
+  max-width: 40em;
+}
+
+.row {
+  justify-content: center;
+}
+</style>
