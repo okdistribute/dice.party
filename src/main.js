e7a32bdec5acee93006f88005752ff9480fa2b4a
Clayton A Davis <cad@clayadavis.net>
2021-08-23 15:04:48 -0400

    Add production build options and deploy script

--- src/main.js
+++ src/main.js
@@ -6,8 +6,14 @@ const app = createApp(App)
 
 app.use(router)
 
-app.config.globalProperties.$messageHost = 'http://localhost:3000'
+// https://cli.vuejs.org/guide/mode-and-env.html#modes
+let msgHost
+if (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test') {
+  msgHost = 'http://localhost:3000'
+} else {
+  // production
+  msgHost = ''
+}
+app.config.globalProperties.$messageHost = msgHost
 
 app.mount('#app')
-
-
